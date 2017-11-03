e66a42a43a3b0408bb80e7d54f21d580adb6e647
kapicaluk <kapicaluk@gmail.com>
2017-11-03 14:42:37 -0400

    single product view

---
+++ client/components/singleProduct.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+export const singleProduct = (props) => {
+  let inventory = props.productList
+  let id = props.match.params.id
+  let singleItem = inventory.filter(item => item.id == id)[0]
+
+  return (
+    <div>
+    <h1> Hai from Jose</h1>
+    {console.log(singleItem)}
+    <h3> {singleItem.name}</h3>
+    <img src={singleItem.imageURL}/>
+    <p> {singleItem.description}</p>
+    <p> {singleItem.price}</p>
+    </div>
+  )
+}
+
+const mapState = (state) => {
+  return {
+    productList: state.products
+  }
+}
+
+export default connect(mapState)(singleProduct)
