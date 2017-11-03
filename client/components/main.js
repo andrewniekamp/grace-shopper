3a56f24506e6adb7ac6cdd186ecf02d263414437
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2017-11-03 13:11:54 -0400

    Add cart badge to navbar to display icon and qty in cart

--- client/components/main.js
+++ client/components/main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {withRouter, Link} from 'react-router-dom'
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
 import store, { logout, products } from "../store";
 
 /**
@@ -11,28 +11,31 @@ import store, { logout, products } from "../store";
  *  rendered out by the component's `children`.
  */
 const Main = (props) => {
-  // console.log('deez nuutz from main', store.getState().products)
-  const {children, handleClick, isLoggedIn} = props
+  const { children, handleClick, isLoggedIn, cart } = props
+  let deepLength = 0;
+  cart.forEach(itemArr => {
+    deepLength += itemArr.length;
+  })
   return (<div>
-      <h1>BOILERMAKER</h1>
-      <nav>
-        {isLoggedIn ? <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
+    <h1>BOILERMAKER</h1>
+    <nav>
+      {isLoggedIn ? <div>
+        {/* The navbar will show these links after you log in */}
+        <Link to="/home">Home</Link>
+        <a href="#" onClick={handleClick}>
+          Logout
             </a>
-          </div> : <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/cart">Cart</Link>
-          </div>}
-      </nav>
-      <hr />
-      {children}
-    </div>);
+      </div> : <div>
+          {/* The navbar will show these links before you log in */}
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/cart"> {deepLength} <span className="glyphicon glyphicon-shopping-cart" aria-hidden="true" /></Link>
+        </div>}
+    </nav>
+    <hr />
+    {children}
+  </div>);
 }
 
 /**
@@ -40,13 +43,14 @@ const Main = (props) => {
  */
 const mapState = (state) => {
   return {
-           isLoggedIn: !!state.user.id
-         }
+    isLoggedIn: !!state.user.id,
+    cart: state.cart
+  }
 }
 
 const mapDispatch = (dispatch) => {
   return {
-    handleClick () {
+    handleClick() {
       dispatch(logout())
     }
   }
