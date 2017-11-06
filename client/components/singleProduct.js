5af1a2dec6388c8c4b2d5e05445de4029c859727
Jose Sabal <jose.sabal@yahoo.com>
2017-11-06 16:41:43 -0500

    Fixed rendering on singleProductView

--- client/components/singleProduct.js
+++ client/components/singleProduct.js
@@ -5,16 +5,16 @@ export const singleProduct = (props) => {
   console.log(props)
   let inventory = props.productList
   let id = props.match.params.id
-  let singleItem = inventory.filter(item => item.id == id)[0]
+  let singleItem = inventory.filter(item => item.id == id)
 console.log("@#@#@!@#!@#!@",singleItem)
   return (
     <div>
     <h1> Hai from Jose</h1>
     {console.log(singleItem)}
-    <h3> {singleItem.name}</h3>
-    <img src={singleItem.imageURL}/>
-    <p> {singleItem.description}</p>
-    <p> {singleItem.price / 100}</p>
+    <h3> {singleItem.length && singleItem[0].name}</h3>
+    <img src={singleItem.length && singleItem[0].imageURL}/>
+    <p> {singleItem.length && singleItem[0].description}</p>
+    <p> {singleItem.length && singleItem[0].price / 100}</p>
     </div>
   )
 }
